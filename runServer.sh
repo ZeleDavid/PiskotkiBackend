50c3451743f2efe2df372d19627923c6ee37b084
David <david.zele@etrust.si>
2022-05-14 19:57:30 +0200

    fix deploy

--- runServer.sh
+++ runServer.sh
@@ -3,11 +3,12 @@ then
     pip install --user virtualenv
     python -m virtualenv v_env
 
+sleep 1m
+
 bash v_env\Scripts\activate
 
 pip install -r requirements.txt
 
-#bash /WAIT /B "Flask backend"
 sleep 1m
 
 set FLASK_APP=server.py
