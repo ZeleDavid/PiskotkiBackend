65d662d774e96f33e3c9697da8e28d49dc0fa4a9
David <david.zele@etrust.si>
2022-05-14 19:36:59 +0200

    deploy fix

---
+++ runServer.sh
@@ -0,0 +1,16 @@
+if [ ! -f v_env\Scripts\activate ]
+then
+    pip install --user virtualenv
+    python -m virtualenv v_env
+
+bash v_env\Scripts\activate
+
+pip install -r requirements.txt
+
+#bash /WAIT /B "Flask backend"
+
+set FLASK_APP=server.py
+set FLASK_ENV=development
+flask run
+
+fi
